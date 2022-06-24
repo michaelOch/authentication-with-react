3608b7764087befffcbfeeaded6317869ed2e223
michaelOch <ochuenwikechukwudi@hotmail.com>
2022-06-25 01:20:46 +0200

    chore: add state to handle form elements in login/register

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function login() {
-  return (
-    <section className='login-section d-flex flex-column justify-content-center align-items-center'>
-        <h3 className='text-center'>Sign In</h3>
-        <form className='w-75'>
-            <div className="form-group mb-3">
-                <input type="email" name="email" className="form-control" placeholder="Email" />
-            </div>
-            <div className="form-group mb-3">
-                <input type="password" name="password" className="form-control" placeholder="Password" />
-            </div>
-            <div className="">
-                <button type='submit' className='btn btn-primary btn-block'>Log In</button>
-            </div>
-        </form>
-    </section>
-  )
+function Login() {
+
+    const initialState = {
+        email: '',
+        password: ''
+    }
+
+    const [data, setData] = useState(initialState);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <section className='login-section d-flex flex-column justify-content-center align-items-center'>
+            {console.log(data)}
+            <h3 className='text-center'>Sign In</h3>
+            <form className='w-75'>
+                <div className="form-group mb-3">
+                    <input 
+                        type="email" 
+                        name="email" 
+                        className="form-control" 
+                        placeholder="Email" 
+                        value={data.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="form-group mb-3">
+                    <input 
+                        type="password" 
+                        name="password" 
+                        className="form-control" 
+                        placeholder="Password" 
+                        value={data.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="">
+                    <button type='submit' className='btn btn-primary btn-block'>Log In</button>
+                </div>
+            </form>
+        </section>
+    )
 }
 
-export default login;
+export default Login;
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function register() {
-  return (
-    <section className='register-section d-flex flex-column justify-content-center align-items-center'>
-        <h3 className='text-center'>Sign Up</h3>
-        <form className='w-75'>
-            <div className='form-group mb-3'>
-                <input type='email' name='email' className='form-control' placeholder='Email' />
-            </div>
-            <div className='form-group mb-3'>
-                <input type='password' name='password' className='form-control' placeholder='Password' />
-            </div>
-            <div className='form-group mb-3'>
-                <input type='password' name='password' className='form-control' placeholder='Confirm Password' />
-            </div>
-            <div className=''>
-                <button type='submit' className='btn btn-primary'>Register</button>
-            </div>
-        </form>
-    </section>
-  )
+function Register() {
+
+    const initialState = {
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+
+    const [data, setData] = useState(initialState);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <section className='register-section d-flex flex-column justify-content-center align-items-center'>
+            {console.log(data)}
+            <h3 className='text-center'>Sign Up</h3>
+            <form className='w-75'>
+                <div className='form-group mb-3'>
+                    <input 
+                        type='email' 
+                        name='email' 
+                        className='form-control' 
+                        placeholder='Email' 
+                        value={data.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='form-group mb-3'>
+                    <input 
+                        type='password' 
+                        name='password' 
+                        className='form-control' 
+                        placeholder='Password' 
+                        value={data.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='form-group mb-3'>
+                    <input 
+                        type='password' 
+                        name='confirmPassword' 
+                        className='form-control' 
+                        placeholder='Confirm Password' 
+                        value={data.confirmPassword}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className=''>
+                    <button type='submit' className='btn btn-primary'>Register</button>
+                </div>
+            </form>
+        </section>
+    )
 }
 
-export default register;
+export default Register;
